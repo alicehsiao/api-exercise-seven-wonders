3830b93a534ccf119d81337c77b6c4fb3f528d1c
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-30 11:31:13 -0700

    Matched example output for seven wonders api exercise

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,15 +1,41 @@
 require 'httparty'
+require 'awesome_print'
 
-#Starter Code:
-seven_wonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
+class SevenWonders
+  attr_accessor :wonder
+
+  def initialize(wonder)
+    @wonder = wonder
+  end
+
+  def get_coordinates
+    coordinates = []
+
+    encoded_uri = URI.encode("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{@wonder}&inputtype=textquery&fields=geometry&key=AIzaSyBP30mYnbwKpZ0lCHtp6FuvcNSjNG0GsGM")
+
+    response = HTTParty.get(encoded_uri)
+
+    parsed_response = response.parsed_response
+
+    lat = parsed_response["candidates"][0]["geometry"]["location"]["lat"]
+    lng = parsed_response["candidates"][0]["geometry"]["location"]["lng"]
+    coordinates << lat
+    coordinates << lng
+
+    return coordinates
+  end
+end
 
 
+coordinate_hash = {}
+seven_wonders = ["Great Pyramid of Giza", "Ishtar Gate", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
 
+seven_wonders.each do |wonder|
+  theWonder = SevenWonders.new(wonder)
+  coordinate_hash[wonder] = {
+    "lat" => theWonder.get_coordinates[0],
+    "lng" => theWonder.get_coordinates[1]
+  }
+end
 
-
-
-
-
-
-#Example Output:
-#{"Great Pyramind of Giza"=>{"lat"=>29.9792345, "lng"=>31.1342019}, "Hanging Gardens of Babylon"=>{"lat"=>32.5422374, "lng"=>44.42103609999999}, "Colossus of Rhodes"=>{"lat"=>36.45106560000001, "lng"=>28.2258333}, "Pharos of Alexandria"=>{"lat"=>38.7904054, "lng"=>-77.040581}, "Statue of Zeus at Olympia"=>{"lat"=>37.6379375, "lng"=>21.6302601}, "Temple of Artemis"=>{"lat"=>37.9498715, "lng"=>27.3633807}, "Mausoleum at Halicarnassus"=>{"lat"=>37.038132, "lng"=>27.4243849}}
+ap coordinate_hash
